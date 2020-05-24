a935dc451e93032df2efc82d829546eb9efa3fee
Aleksandar Apostolov <a_aleksandar86@yahoo.com>
2020-05-24 20:02:29 +0200

    Show view, route and definition function added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tweets#index'
-  get 'tweets/index'
   get '/tweets', to: 'tweets#index'
+  get '/tweets/:id', to: 'tweets#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
