bd7bd6799ad1080c43660a1cd3e0f68fb066494a
Aleksandar Apostolov <a_aleksandar86@yahoo.com>
2020-05-24 20:52:23 +0200

    New and create actions implemented

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,3 @@
 class Tweet < ApplicationRecord
+  validates :body, presence: true, length: { maximum: 40 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'tweets#index'
   get '/tweets', to: 'tweets#index'
-  get '/tweets/:id', to: 'tweets#show'
+  get '/tweets/:id', to: 'tweets#show', as: :show_tweet
+  get '/tweets/new', to: 'tweets#new', as: :new_tweet
+  post '/tweets', to: 'tweets#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
