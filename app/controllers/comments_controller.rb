4abf918f14b5c3fc95e37e829e78027d046f20ff
Aleksandar Apostolov <a_aleksandar86@yahoo.com>
2020-07-13 01:59:56 +0200

    Comments actions, new, edit and delete comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,48 @@
+class CommentsController < ApplicationController
+  def new
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment = @tweet.comments.build
+  end
+
+  def create
+    @tweet = Tweet.find(params[:tweet_id])
+
+    @comment = @tweet.comments.build(comment_params)
+
+    if @comment.save
+      redirect_to @tweet
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+
+    redirect_to comment.tweet
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+
+    @tweet = @comment.tweet
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @tweet = @comment.tweet
+
+    if @comment.update(comment_params)
+      redirect_to @tweet
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+end
